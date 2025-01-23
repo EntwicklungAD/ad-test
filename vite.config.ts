1b9ea7d37550cc8058607ae2267078fab8a09b8d
sro <simon.rohde@dbh.de>
2025-01-23 13:17:13 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/entwicklungad.github.io/",
+  base: "/ad-test/",
   plugins: [react()],
 })
